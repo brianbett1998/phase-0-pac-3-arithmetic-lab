e1622cd4efe447e7bad73b630eb527eba275f3a5
BRIAN BETT <+>
2023-03-11 21:11:36 +0300

    Done with assignment

---
+++ index.js
@@ -0,0 +1,22 @@
+1 + 80; //=> 81)
+60 - 40; //=> 20
+2*3.4; //=> 6.8
+5.0 / 2.5; // => 2
+function add(a,b) {return a+b}  
+function subtract(c,d){return c-d}    
+function multiply(e,f){return e*f}
+function divide(g,h){return g/h}
+increment(n)
+    var number = 5;
+    console.log(number++)
+    result;//=> 6--the numer was increased after it was evaluated    
+
+decrement (n)
+    var number = 6
+    console.log(number--)
+    number;//=> 5-- the number was decreased after it was evalated}
+parseInt('n',10)
+preserveDecimal(n);{return}
+parseFloat("n");//n;
+makInt:""
+preserveDecimal:""
